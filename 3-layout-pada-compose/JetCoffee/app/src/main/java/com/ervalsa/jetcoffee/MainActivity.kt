68a3dc3862d30240ba62aeb1edb744350ad8d44e
ervalsa <ervalsa-san@gmail.com>
2022-11-01 00:03:12 +0800

    Add Section Category Row

--- 3-layout-pada-compose/JetCoffee/app/src/main/java/com/ervalsa/jetcoffee/MainActivity.kt
+++ 3-layout-pada-compose/JetCoffee/app/src/main/java/com/ervalsa/jetcoffee/MainActivity.kt
@@ -4,20 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ervalsa.jetcoffee.model.dummyCategory
+import com.ervalsa.jetcoffee.ui.components.CategoryItem
 import com.ervalsa.jetcoffee.ui.components.SearchBar
 import com.ervalsa.jetcoffee.ui.theme.JetCoffeeTheme
 
@@ -34,9 +38,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp() {
-    Column() {
+fun JetCoffeeApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
         Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -63,4 +71,42 @@ fun Banner(
         )
         SearchBar()
     }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        modifier = modifier.padding(top = 8.dp, bottom = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryRowPreview() {
+    JetCoffeeTheme {
+        CategoryRow()
+    }
+}
+
+@Composable
+fun SectionText(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.h5.copy(
+            fontWeight = FontWeight.ExtraBold
+        ),
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    )
 }
---
+++ 3-layout-pada-compose/JetCoffee/app/src/main/java/com/ervalsa/jetcoffee/ui/components/CategoryItem.kt
@@ -0,0 +1,62 @@
+package com.ervalsa.jetcoffee.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ervalsa.jetcoffee.R
+import com.ervalsa.jetcoffee.model.Category
+import com.ervalsa.jetcoffee.ui.theme.JetCoffeeTheme
+
+@Composable
+fun CategoryItem(
+    category: Category,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(category.imageCategory),
+            contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(category.textCategory),
+            fontSize = 10.sp,
+            modifier = Modifier.paddingFromBaseline(
+                top = 16.dp,
+                bottom = 8.dp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryItemPreview() {
+    JetCoffeeTheme {
+        CategoryItem(
+            category = Category(
+                R.drawable.icon_category_cappuccino,
+                R.string.category_cappuccino
+            ),
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
+}
