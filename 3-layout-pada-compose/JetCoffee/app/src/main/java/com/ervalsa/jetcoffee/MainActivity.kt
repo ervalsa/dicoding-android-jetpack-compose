f9601aeee6b0a1a449cdfd3ef77626089d8dc2f8
ervalsa <ervalsa-san@gmail.com>
2022-10-31 23:50:29 +0800

    Add Banner Components

--- 3-layout-pada-compose/JetCoffee/app/src/main/java/com/ervalsa/jetcoffee/MainActivity.kt
+++ 3-layout-pada-compose/JetCoffee/app/src/main/java/com/ervalsa/jetcoffee/MainActivity.kt
@@ -3,14 +3,22 @@ package com.ervalsa.jetcoffee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ervalsa.jetcoffee.ui.components.SearchBar
 import com.ervalsa.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +35,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-
+    Column() {
+        Banner()
+    }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
@@ -35,5 +45,22 @@ fun JetCoffeeApp() {
 fun JetCoffeeAppPreview() {
     JetCoffeeTheme {
         JetCoffeeApp()
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
     }
 }
