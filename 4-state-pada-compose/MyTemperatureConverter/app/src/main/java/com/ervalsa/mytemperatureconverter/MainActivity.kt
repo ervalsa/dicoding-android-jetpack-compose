e0370a9e0deea2e081ab0d747e3e974203162011
ervalsa <ervalsa-san@gmail.com>
2022-11-01 14:46:40 +0800

    Add Stateful, Stateless Converter Temperature

--- 4-state-pada-compose/MyTemperatureConverter/app/src/main/java/com/ervalsa/mytemperatureconverter/MainActivity.kt
+++ 4-state-pada-compose/MyTemperatureConverter/app/src/main/java/com/ervalsa/mytemperatureconverter/MainActivity.kt
@@ -3,13 +3,24 @@ package com.ervalsa.mytemperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import com.ervalsa.mytemperatureconverter.ui.theme.MyTemperatureConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        StatefulTemperaturInput()
+                        ConverterApp()
+                        TwoWayConverterApp()
+                    }
                 }
             }
         }
@@ -30,14 +45,138 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StatefulTemperaturInput(
+    modifier: Modifier = Modifier
+) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+
+    Column(
+        modifier.padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            },
+        )
+        Text(
+            text = stringResource(R.string.temperature_fahrenheit, output)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MyTemperatureConverterTheme {
-        Greeting("Android")
+fun StatelessTemperaturInput(
+    input: String,
+    output: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier.padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.stateless_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
     }
+}
+
+@Composable
+private fun ConverterApp(
+    modifier: Modifier = Modifier,
+) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+
+    Column(
+        modifier
+    ) {
+        StatelessTemperaturInput(
+            input = input,
+            output = output,
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            }
+        )
+    }
+}
+
+@Composable
+fun GeneralTemperatureInput(
+    scale: Scale,
+    input: String,
+    onValueChange: (String) -> Unit,
+    modifier:Modifier = Modifier
+) {
+    Column(modifier) {
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_temperature, scale.scaleName)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange
+        )
+    }
+}
+
+@Composable
+private fun TwoWayConverterApp(
+    modifier: Modifier = Modifier
+) {
+    var celcius by remember { mutableStateOf("") }
+    var fahrenheit by remember { mutableStateOf("") }
+
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.two_way_converter),
+            style = MaterialTheme.typography.h5
+        )
+        GeneralTemperatureInput(
+            scale = Scale.CELCIUS,
+            input = celcius,
+            onValueChange = { newInput ->
+                celcius = newInput
+                fahrenheit = convertToFahrenheit(newInput)
+            }
+        )
+        GeneralTemperatureInput(
+            scale = Scale.FAHRENHEIT,
+            input = fahrenheit,
+            onValueChange = { newInput ->
+                fahrenheit = newInput
+                celcius = convertToCelcius(newInput)
+            }
+        )
+    }
+}
+
+fun convertToCelcius(fahrenheit: String) =
+    fahrenheit.toDoubleOrNull()?.let {
+        (it - 32) * 5 / 9
+    }.toString()
+
+fun convertToFahrenheit(celcius: String) =
+    celcius.toDoubleOrNull()?.let {
+        (it * 9 / 5) + 32
+    }.toString()
+
+enum class Scale(val scaleName: String) {
+    CELCIUS("Celcius"),
+    FAHRENHEIT("Fahrenheit")
 }
