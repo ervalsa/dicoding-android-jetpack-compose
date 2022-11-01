20565598b40ad1e436656321968849f5ce60cb5f
ervalsa <ervalsa-san@gmail.com>
2022-11-01 17:11:07 +0800

    Add Lazy Column

--- 5-lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/ervalsa/jetheroes/JetHeroesApp.kt
+++ 5-lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/ervalsa/jetheroes/JetHeroesApp.kt
@@ -1,9 +1,13 @@
 package com.ervalsa.jetheroes
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ervalsa.jetheroes.model.HeroesData
 import com.ervalsa.jetheroes.ui.theme.JetHeroesTheme
 
 @Composable
@@ -13,7 +17,15 @@ fun JetHeroesApp(
     Box(
         modifier = modifier
     ) {
-
+        LazyColumn {
+            items(HeroesData.heroes, key = { it.id }) { hero ->
+                HeroListItem(
+                    name = hero.name,
+                    photoUrl = hero.photoUrl,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
     }
 }
 
--- 5-lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/ervalsa/jetheroes/MainActivity.kt
+++ 5-lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/ervalsa/jetheroes/MainActivity.kt
@@ -3,13 +3,21 @@ package com.ervalsa.jetheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.ervalsa.jetheroes.ui.theme.JetHeroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,5 +34,46 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun HeroListItem(
+    name: String,
+    photoUrl: String,
+    modifier: Modifier = Modifier
+) {
+     Row(
+         verticalAlignment = Alignment.CenterVertically,
+         modifier = modifier.clickable {  }
+     ) {
+         AsyncImage(
+             model = photoUrl,
+             contentDescription = null,
+             contentScale = ContentScale.Crop,
+             modifier = Modifier
+                 .padding(8.dp)
+                 .size(60.dp)
+                 .clip(CircleShape)
+         )
+         Text(
+             text = name,
+             fontWeight = FontWeight.Medium,
+             modifier = Modifier
+                 .fillMaxWidth()
+                 .weight(1f)
+                 .padding(start = 16.dp)
+         )
+     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeroListItemPreview() {
+    JetHeroesTheme {
+        HeroListItem(
+            name = "H.O.S. Cokroaminoto",
+            photoUrl = ""
+        )
     }
 }
