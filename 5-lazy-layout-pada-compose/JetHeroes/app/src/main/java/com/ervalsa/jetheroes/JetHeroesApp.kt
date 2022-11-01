dd24f277506c93ba9b16b09583c0511d61607348
ervalsa <ervalsa-san@gmail.com>
2022-11-01 16:37:18 +0800

    Init JetHeroes Project

---
+++ 5-lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/ervalsa/jetheroes/JetHeroesApp.kt
@@ -0,0 +1,26 @@
+package com.ervalsa.jetheroes
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ervalsa.jetheroes.ui.theme.JetHeroesTheme
+
+@Composable
+fun JetHeroesApp(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetHeroesAppPreview() {
+    JetHeroesTheme {
+        JetHeroesApp()
+    }
+}
