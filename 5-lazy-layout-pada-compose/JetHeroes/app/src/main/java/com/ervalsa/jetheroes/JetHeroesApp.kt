2a14243db25ff889de7ee3c12b1a5a91ad8aa325
ervalsa <ervalsa-san@gmail.com>
2022-11-01 17:22:04 +0800

    Add Scroll to Top with Button

--- 5-lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/ervalsa/jetheroes/JetHeroesApp.kt
+++ 5-lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/ervalsa/jetheroes/JetHeroesApp.kt
@@ -1,14 +1,33 @@
 package com.ervalsa.jetheroes
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import com.ervalsa.jetheroes.model.HeroesData
 import com.ervalsa.jetheroes.ui.theme.JetHeroesTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun JetHeroesApp(
@@ -17,7 +36,16 @@ fun JetHeroesApp(
     Box(
         modifier = modifier
     ) {
-        LazyColumn {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton: Boolean by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 }
+        }
+
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 80.dp)
+        ) {
             items(HeroesData.heroes, key = { it.id }) { hero ->
                 HeroListItem(
                     name = hero.name,
@@ -26,6 +54,48 @@ fun JetHeroesApp(
                 )
             }
         }
+        AnimatedVisibility(
+            visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            ScrollTopButton(
+                onClick = {
+                    scope.launch {
+                        listState.scrollToItem(index = 0)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun ScrollTopButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .shadow(
+                elevation = 10.dp,
+                shape = CircleShape
+            )
+            .clip(shape = CircleShape)
+            .size(56.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.White,
+            contentColor = MaterialTheme.colors.primary
+        )
+    ) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = null
+        )
     }
 }
 
