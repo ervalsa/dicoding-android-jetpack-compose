5b0db093918dcb0b85ffc32a05f11b70b385eef7
ervalsa <ervalsa-san@gmail.com>
2022-11-02 13:54:29 +0800

    Init Compose Testing Project

---
+++ 7-testing-pada-compose/MyComposeTesting/app/src/main/java/com/ervalsa/mycomposetesting/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ervalsa.mycomposetesting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ervalsa.mycomposetesting.ui.theme.MyComposeTestingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeTestingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyComposeTestingTheme {
+        Greeting("Android")
+    }
+}
